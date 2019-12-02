0d9a06d7d2221ab65594b60830a9aa37cc545975
Максим Шаров <sm710mh@gmail.com>
2019-12-02 12:39:25 +0300

    add style.css f:minimize

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,14 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('minimizecss', function () {
+    gulp.src('/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
