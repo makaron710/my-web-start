27cb30066f370fc1baed4a01ceaa20c00b2e77aa
Максим Шаров <sm710mh@gmail.com>
2019-12-04 18:48:58 +0300

    feat: add task 'Compile sass into CSS' & refact: upgrate syntax on gulpfile to v.4 & doc: add comment on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
-var gulp        = require('gulp');
+var {src, dest, watch}        = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var cssmin      = require('gulp-cssmin');
+var rename      = require('gulp-rename');
 
-// Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
+// Compile sass into CSS & auto-inject into browsers | компилятор sass в css
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+// Static server | сервер для авто обновления страницы
+function bs() {
+    serveSass();
+     browserSync.init({
       server: {
           baseDir: "./"
       }
-  });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+};
 
-
-var cssmin = require('gulp-cssmin');
-var rename = require('gulp-rename');
- 
-gulp.task('minimizecss', function () {
-    return gulp.src('dist/*.css')
+//minimize css | минимизация css
+function minCss() {
+    return src('./css/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(dest('css'));
+};
+
+exports.bs = bs; //вызов таска командой gulp bs
+exports.minCss = minCss;
