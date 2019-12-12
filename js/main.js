437d5251698913263842d12cd6f12f348d17db39
Максим Шаров <sm710mh@gmail.com>
2019-12-12 19:43:09 +0300

    refact: add jquery function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-//проверка прогрузки html
+/* //проверка прогрузки html
 document.addEventListener("DOMContentLoaded", function(event){
+
   //объект документа с классом modal
   const modal = document.querySelector('.modal'); 
   //все объекты документа с селектором data-toggle=modal
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function(event){
   const closeBtn = document.querySelector('.modal__close'); 
   // объект документа с класссом modal__over
   const closeOver = document.querySelector('.modal__over');
+
   //функция toggle переключает наличие/отсутствие класса
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
@@ -20,12 +22,52 @@ document.addEventListener("DOMContentLoaded", function(event){
   
   // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
   closeBtn.addEventListener('click', switchModal);
+
   // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
   closeOver.addEventListener('click', switchModal);
+
   // при нажатии клавиши escape, открытие/закрытие вызов функции switchModal
   document.addEventListener('keydown', (event) => {
     if (event.keyCode == 27) {
       modal.classList.toggle('modal--visible');
     }
   });
+}); */
+
+//jQuery
+//проверка прогрузки html
+$(document).ready(function(){
+
+  //объект документа с классом modal
+  var modal = $('.modal'),
+  //все объекты документа с селектором data-toggle=modal
+      modalBtn = $('[data-toggle=modal]'),
+  // объект документа с класссом modal_close
+      closeBtn = $('.modal__close'),
+  // объект документа с класссом modal__over
+      closeOver = $('.modal__over');
+
+  //функция toggle переключает наличие/отсутствие класса
+    var switchModal = () => {
+    modal.toggleClass('modal--visible');
+  };
+
+  //для modalBtn отслеживать событие "клик"
+  //при наступлении вызвать функццию switchModal чтобы открыть окно
+  modalBtn.on('click', switchModal);
+  
+  // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
+  closeBtn.on('click', switchModal);
+
+  // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
+  closeOver.on('click', switchModal);
+
+  // при нажатии клавиши escape - удаление класса .modal--visible
+  $(document).keyup(function(e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+      modal.removeClass('modal--visible');
+    }
+  });
+  
+  
 });
