ecdf75ffe850b40e4e4eba9ddd7362619ce9bec6
Максим Шаров <sm710mh@gmail.com>
2019-12-12 18:42:22 +0300

    doc: main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event){  /* проверка чтобы все прогрузилось */
-  
-  const modal = document.querySelector('.modal'); /* объект документа с селектором modal */
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]'); /* все объекты документа с селектором data-toggle=modal */
-  const closeBtn = document.querySelector('.modal__close'); /* объект документа с селектором modal_close */
+//проверка прогрузки html
+document.addEventListener("DOMContentLoaded", function(event){
+  //объект документа с классом modal
+  const modal = document.querySelector('.modal'); 
+  //все объекты документа с селектором data-toggle=modal
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  // объект документа с класссом modal_close
+  const closeBtn = document.querySelector('.modal__close'); 
+  // объект документа с класссом modal__over
   const closeOver = document.querySelector('.modal__over');
-    
+  //функция toggle переключает наличие/отсутствие класса
   const switchModal = () => {
-    modal.classList.toggle('modal--visible'); /* функция toggle переключает наличие/отсутствие класса */
+    modal.classList.toggle('modal--visible');
   }
-
-  modalBtn.forEach(element => { /* для каждого modalBtn отслеживать элементы с событием "клик" */
-    element.addEventListener('click', switchModal); /* при наступлении вызвать функццию switchModal чтобы открыть окно*/
+  //для каждого modalBtn отслеживать элементы с событием "клик"
+  //при наступлении вызвать функццию switchModal чтобы открыть окно
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
   });
   
-
-  closeBtn.addEventListener('click', switchModal); /* при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно*/
-
+  // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
+  closeBtn.addEventListener('click', switchModal);
+  // при клике по объекту closeBtn вызвать функццию switchModal чтобы закрыть окно
   closeOver.addEventListener('click', switchModal);
-  
-  document.addEventListener("keydown", function(event) {
+  // при нажатии клавиши escape, открытие/закрытие вызов функции switchModal
+  document.addEventListener('keydown', (event) => {
     if (event.keyCode == 27) {
       modal.classList.toggle('modal--visible');
     }
   });
-
-
 });
