0b0b217783fa185ecb1363aeff811070bab9544d
Максим Шаров <sm710mh@gmail.com>
2020-01-05 19:38:24 +0300

    page spreed 80+ & minimize file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const {src, dest, watch}        = require('gulp');
+const {src, dest, watch, series}        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
 const cssmin      = require('gulp-cssmin');
 const rename      = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Compile sass into CSS & auto-inject into browsers | компилятор sass в css
 function serveSass() {
@@ -29,12 +33,69 @@ function bs() {
 };
 
 //minimize css | минимизация css
-function minCss() {
+/* function minCss() {
     return src('./css/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(dest('css'));
+}; */
+//Будет использована clean css
+
+//Подготовка пронкта к выгрузке (сжатие, минимификация...)
+//CSS
+function buildCSS(done) {
+    src('css/**/**.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('dist/css/'));
+    done();
+};
+//JS
+function buildJS(done) {
+    src(['js/**/**.js', '!js/**/**.min.js'])
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            }
+        }))
+        .pipe(dest('dist/js/'));
+    src('js/**/**.min.js')
+        .pipe(dest('dist/js/'));
+    done();
+};
+
+function buildHTML(done) {
+    src('**.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist/'));
+    done();
+};
+
+function php(done) {
+    src('**.php')
+        .pipe(dest('dist/'));
+    src('phpmailer/**/**')
+        .pipe(dest('dist/phpmailer/'));
+    done();
+};
+
+function fonts(done) {
+    src('css/fonts/**/**')
+        .pipe(dest('dist/css/fonts'));
+    done();
+};
+
+function imgMin(done) {
+    src('img/**/**')
+        .pipe(tinypng({
+            key: 'h0pJDThTqVTvbqb1CHhpPMrFlsfs7rpY'
+        }))
+        .pipe(dest('dist/img/'));
+    src(['img/**/**.svg', '!img/**/**.{png,jpg,jpeg}'])
+        .pipe(dest('dist/img/'));
+    done();
 };
 
 exports.bs = bs; //вызов таска командой gulp bs
-exports.minCss = minCss;
+//exports.minCss = minCss;
+exports.build = series(buildCSS, buildJS, buildHTML, php, fonts);
+exports.imgmin = imgMin;
