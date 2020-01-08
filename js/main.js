d28a36608a7939e0aa925c23b48e2b5377398bf2
Максим Шаров <sm710mh@gmail.com>
2020-01-08 19:48:56 +0300

    refact: correct for mobile

--- js/main.js
+++ js/main.js
@@ -186,6 +186,7 @@ $(document).ready(function(){
 
   // Маска для телефона
   $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: "+7(___) ___-__-__"});
+  $('[type=tel-text-mask]').mask('+7(000) 000-00-00');
 
 // End
 });
